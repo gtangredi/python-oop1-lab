9ab40465f53e9db308ed8a66894ce392cc0c4fc9
gtangredi <gtangredi315@gmail.com>
2026-02-13 10:19:26 -0500

    complete book coffee lesson

--- lib/book.py
+++ lib/book.py
@@ -1,6 +1,14 @@
 #!/usr/bin/env python3
 
 class Book:
-    pass
-    
-        
+    page_count = 0
+
+    def __init__(self, title, page_count):
+        self.title = title
+
+        if not isinstance(page_count, int):
+            raise TypeError("page_count must be an integer")
+        self.page_count = page_count
+
+    def turn_page(self):
+        print("Flipping the page...wow, you read fast!")
--- lib/coffee.py
+++ lib/coffee.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
 
 class Coffee:
-    pass
+    size = "Medium"
+
+    def __init__(self, size, price):
+        if size not in ["Small", "Medium", "Large"]:
+            raise ValueError("size must be 'Small', 'Medium', or 'Large'")
+        self.size = size
+
+        if not isinstance(price, (int, float)):
+            raise TypeError("price must be a number")
+        self.price = price
+
+    def tip(self):
+        print("This coffee is great, here’s a tip!")
+        self.price += 1
